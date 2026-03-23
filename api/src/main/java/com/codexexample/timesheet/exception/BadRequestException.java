0834784c8420b927ec90e26272f710adf35677f7
dpitt <dpitt@keyholesoftware.com>
2026-03-23 16:53:42 -0500

    removed

--- api/src/main/java/com/codexexample/timesheet/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.codexexample.timesheet.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- api/src/main/java/com/codexexample/timesheet/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.codexexample.timesheet.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
