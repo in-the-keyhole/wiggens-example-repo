a08961d904113bb7502895a8d7dbbe823e1240ce
dpitt <dpitt@keyholesoftware.com>
2026-03-23 15:55:01 -0500

    update

--- api/src/main/java/com/example/timesheet/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.timesheet.repository;
-
-import com.example.timesheet.domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    boolean existsByCode(String code);
-}
-
--- api/src/main/java/com/example/timesheet/repository/TimesheetEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.timesheet.repository;
-
-import com.example.timesheet.domain.TimesheetEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimesheetEntryRepository extends JpaRepository<TimesheetEntry, Long> {
-}
-
