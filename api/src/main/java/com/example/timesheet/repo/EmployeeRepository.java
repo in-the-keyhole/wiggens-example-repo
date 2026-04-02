61736372dc4617548b467e895fec87f684a17630
dpitt <dpitt@keyholesoftware.com>
2026-04-02 18:55:31 +0200

    update

--- api/src/main/java/com/example/timesheet/repo/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.timesheet.repo;
-
-import com.example.timesheet.domain.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
-
