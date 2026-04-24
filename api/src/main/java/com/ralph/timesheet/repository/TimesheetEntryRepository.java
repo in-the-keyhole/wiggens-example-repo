0a6f94526c77195b7a077ad62ae5c003f2d213e5
dpitt <dpitt@keyholesoftware.com>
2026-04-24 15:55:39 -0500

    refresh

--- api/src/main/java/com/ralph/timesheet/repository/TimesheetEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ralph.timesheet.repository;
-
-import com.ralph.timesheet.model.TimesheetEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimesheetEntryRepository extends JpaRepository<TimesheetEntry, Long> {
-}
-
